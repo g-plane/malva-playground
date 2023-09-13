b81f6ff6cb481939e418ca1d950c3c592e8161c7
Pig Fang <g-plane@hotmail.com>
2023-09-13 09:57:46 +0800

    load Monaco Editor workers

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,32 @@ import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 import App from './App.svelte'
 
+self.MonacoEnvironment = {
+  async getWorker(_, label) {
+    switch (label) {
+      case 'css': {
+        const cssWorker = await import(
+          'monaco-editor/esm/vs/language/css/css.worker?worker'
+        )
+        return new cssWorker.default()
+      }
+      case 'json':
+      case 'jsonc': {
+        const jsonWorker = await import(
+          'monaco-editor/esm/vs/language/json/json.worker?worker'
+        )
+        return new jsonWorker.default()
+      }
+      default: {
+        const editorWorker = await import(
+          'monaco-editor/esm/vs/editor/editor.worker?worker'
+        )
+        return new editorWorker.default()
+      }
+    }
+  },
+}
+
 const app = new App({
   target: document.querySelector('#app')!,
 })
